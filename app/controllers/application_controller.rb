458cf31e8384af8685266ad37d52e5883e302e33
Noah Reece <noah.w.reece@gmail.com>
2022-04-18 16:16:17 -0400

    post new word call

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,16 @@ class ApplicationController < Sinatra::Base
   get '/' do
     { message: 'Good luck with your project!' }.to_json
   end
+
+  post '/word' do
+    text = params[:text]
+    image_id = params[:image_id]
+    user_ip = params[:user_ip]
+
+    image = Image.find(image_id)
+    user = User.find_by_ip_address(user_ip)
+
+    word = Word.create(text: text, user: user, image: image)
+    word.to_json
+  end
 end
