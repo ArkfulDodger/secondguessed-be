1c047d61e088731f274ba057162123e437000b8d
Noah Reece <noah.w.reece@gmail.com>
2022-04-18 16:27:31 -0400

    updated seed ref names

--- db/seeds.rb
+++ db/seeds.rb
@@ -101,18 +101,18 @@ u5 = User.create(name: 'Lurker', ip_address: '000.000.0.4')
 puts '...Done seeding users'
 
 puts 'Seeding Words...'
-w1 = Word.create(text: 'fire', image: img, user: u1)
-w2 = Word.create(text: 'lava', image: img, user: u2)
-w3 = Word.create(text: 'painting', image: img, user: u3)
-w4 = Word.create(text: 'squiggly', image: img, user: u4)
+w1 = Word.create(text: 'fire', image: img, submitter: u1)
+w2 = Word.create(text: 'lava', image: img, submitter: u2)
+w3 = Word.create(text: 'painting', image: img, submitter: u3)
+w4 = Word.create(text: 'squiggly', image: img, submitter: u4)
 puts '...Done seeding Words!'
 
 puts 'Seeding Guesses...'
-g1 = Guess.create(image: img, user: u1, word: w2)
-g2 = Guess.create(image: img, user: u2, word: w2)
-g3 = Guess.create(image: img, user: u3, word: w3)
-g4 = Guess.create(image: img, user: u4, word: w3)
-g5 = Guess.create(image: img, user: u5, word: w3)
+g1 = Guess.create(image: img, guesser: u1, guessed_word: w2)
+g2 = Guess.create(image: img, guesser: u2, guessed_word: w2)
+g3 = Guess.create(image: img, guesser: u3, guessed_word: w3)
+g4 = Guess.create(image: img, guesser: u4, guessed_word: w3)
+g5 = Guess.create(image: img, guesser: u5, guessed_word: w3)
 puts '...Done seeding Guesses!'
 
 puts '🌱 ..... All Done Seeding! 🌱'
