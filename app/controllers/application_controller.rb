2dc50b8f4ce9949fadfbd6b21f42ebeff84bdce7
Noah Reece <noah.w.reece@gmail.com>
2022-04-19 12:27:53 -0400

    full crud requests for guess selection, updates, and removal

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,4 +24,42 @@ class ApplicationController < Sinatra::Base
     words = image.submitted_words
     words.to_json
   end
+
+  post '/guesses' do
+    image_id = params[:image_id]
+    word_id = params[:word_id]
+    user_ip = params[:user_IP]
+
+    user = User.find_by_ip_address(user_ip)
+    image = Image.find(image_id)
+    word = Word.find(word_id)
+
+    # binding.pry
+    guess = Guess.create(image: image, guessed_word: word, guesser: user)
+
+    guess.to_json
+  end
+
+  get '/current-guess/:image_id/:user_ip' do
+    image_id = params[:image_id]
+    user_ip = params[:user_ip]
+
+    user = User.find_by_ip_address(user_ip)
+    image = Image.find(image_id)
+
+    guess = Guess.find_by(guesser: user, image: image)
+    guess.to_json
+  end
+
+  patch '/guesses/:id' do
+    guess = Guess.find(params[:id])
+    guess.update(word_id: params[:word_id])
+    guess.to_json
+  end
+
+  delete '/guesses/:id' do
+    guess = Guess.find(params[:id])
+    guess.destroy
+    guess.to_json
+  end
 end
